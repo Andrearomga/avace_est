4b3c2cdcddf383b193fdb10be2b5bb30ddd4c9b5
Andrearomga <211132@ids.upchiapas.edu.mx>
2024-11-05 22:41:43 -0600

    Reporte F-auditoria

--- web_mysonda/obj/Debug/net8.0/web_mysonda.AssemblyInfo.cs
+++ web_mysonda/obj/Debug/net8.0/web_mysonda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web_mysonda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6500b1534612e0d4274125bbc92c717cbc1e01af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+919a2a7ba1411f4f86ddf887bf20324752b19112")]
 [assembly: System.Reflection.AssemblyProductAttribute("web_mysonda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web_mysonda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
